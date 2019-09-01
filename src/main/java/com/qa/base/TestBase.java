c824ea918723b28896e2eab908bf3dd3423724cb
rushi2828 <rushi2828@gmail.com>
2019-09-01 14:26:43 +0530

    Added project structure

---
+++ src/main/java/com/qa/base/TestBase.java
@@ -0,0 +1,52 @@
+package com.qa.base;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.qa.util.TestUtil;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	public static Properties properties;
+	
+	public TestBase() throws IOException {
+		properties = new Properties();
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/main/java/com/qa/config/config.properties");
+		properties.load(fis);
+	}
+	
+	public static void initialisation() {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		// This line is for browser 
+		driver = new ChromeDriver();
+		
+		// below code is for headless browser
+		/*ChromeOptions options = new ChromeOptions();		
+		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--disable-extensions");
+		options.setExperimentalOptions("useAutomationExtension", false);
+		options.addArguments("--proxy-server='direct://'");
+		options.addArguments("--proxy-bypass-list=*");
+		options.addArguments("--start-maximized");
+		options.addArguments("--headless");	 
+		driver = new ChromeDriver(options);*/
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		
+		driver.get(properties.getProperty("url"));
+	}
+
+}
